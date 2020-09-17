913a8c6cbd742a38feb4b35ec2ad66518ef40c08
POWER Alexander <71255126+apowapow@users.noreply.github.com>
2020-09-17 16:02:47 +0100

    get lon lat

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import requests
+import json
+from typing import Tuple
+
+
+API_POSTCODES = "http://api.postcodes.io/postcodes/{0}"
+API_TRANSPORT = "http://transportapi.com"
+
+
 def main():
-    pass
+    print(get_lon_lat("postcode"))
+
+
+def get_lon_lat(postcode: str) -> Tuple:
+    resp = requests.get(url=API_POSTCODES.format(postcode))
+
+    if 200 <= resp.status_code <= 299:
+        data = resp.json()
+        return data["result"]["longitude"], data["result"]["latitude"]
+    else:
+        raise Exception("Failed to get lon/lat: status code {0}".format(resp.status_code))
 
 
 if __name__ == "__main__":
