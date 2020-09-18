d764947706f4d3c465005a48948accf5769b6e37
daisypoole <71255075+daisypoole@users.noreply.github.com>
2020-09-18 11:38:48 +0100

    display bus times on web

--- main.py
+++ main.py
@@ -13,12 +13,19 @@ def index():
 
 @app.route("/busInfo")
 def busInfo():
-    # lat, lon = bus.get_lat_lon(args.postcode)
-    # bus_stops = bus.get_bus_stops(args.app_id, args.app_key, lat, lon)
-    # departures = bus.get_next_buses(args.app_id, args.app_key, bus_stops[0])
 
     postcode = request.args.get('postcode')
-    return render_template('info.html', postcode=postcode)
+
+    lat, lon = bus.get_lat_lon(postcode)
+    bus_stops = bus.get_bus_stops(args.app_id, args.app_key, lat, lon)
+    departures_closest = bus.get_next_buses(args.app_id, args.app_key, bus_stops[0])
+
+    buses_closest = [(dep["operator_name"], dep["aimed_departure_time"]) for dep in departures_closest]
+    bus_stop_names = [(stop["name"], stop["distance"], buses_closest) for stop in bus_stops]
+    #bus_info = [stop['distance', 'description']]
+
+
+    return render_template('info.html', postcode=postcode, bus_stop_names=bus_stop_names)
 
 
 if __name__ == "__main__":
